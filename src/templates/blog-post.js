71fbe93b03fa59cb570eb95e389d81cca724ebcf
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-15 19:50:37 +0100

    Fix file import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 const BlogPostTemplate = ({ data }) => {
   const { bgImage } = data.landingPage.frontmatter
