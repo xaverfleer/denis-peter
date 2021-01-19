871c5628bc67b50923afd6d1b15c9a3bea089104
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-19 15:17:42 +0100

    Add /bibliothek

---
+++ content/pages/library.md
@@ -0,0 +1,5 @@
+---
+templateKey: libraryPage
+title: Bibliothek
+intro: Vertiefen Sie Ihr Wissen zu Permakultur? Sind sie neugierig, etwas neues zu lernen? In dieser Bibliothe finden Sie Informationen rund um die Permakultur.
+---
---
+++ src/pages/bibliothek.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+const HomeIndex = ({ data }) => {
+  const { frontmatter: landingPageFrontmatter } = data.landingPage
+  const { bgImage, meImage, contact } = landingPageFrontmatter
+
+  const { frontmatter: libraryPageFrontmatter } = data.libraryPage
+  const siteTitle = libraryPageFrontmatter.title
+  const { intro } = libraryPageFrontmatter
+
+  return (
+    <Layout
+      bgFluid={bgImage.childImageSharp.fluid}
+      email={contact.email}
+      meFixed={meImage.childImageSharp.fixed}
+    >
+      <Helmet>
+        <title>Permakultur Handwerk | Bibliothek</title>
+        <meta
+          name="description"
+          content="Bibliothek mit Wissen Rund um die Permakultur."
+        />
+      </Helmet>
+
+      <div id="main">
+        <section>
+          <header className="major">
+            <h1>{siteTitle}</h1>
+          </header>
+          <p>{intro}</p>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+export default HomeIndex
+
+export const pageQuery = graphql`
+  query {
+    libraryPage: markdownRemark(
+      frontmatter: { templateKey: { eq: "libraryPage" } }
+    ) {
+      frontmatter {
+        title
+        intro
+      }
+    }
+    landingPage: markdownRemark(
+      frontmatter: { templateKey: { eq: "landingPage" } }
+    ) {
+      frontmatter {
+        bgImage {
+          childImageSharp {
+            fluid(maxWidth: 1800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        meImage {
+          childImageSharp {
+            fixed(width: 200, height: 200) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+        contact {
+          email
+        }
+      }
+    }
+  }
+`
